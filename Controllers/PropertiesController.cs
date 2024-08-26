fc961ae08fdbd330f1eada0497901c12c8e4a954
brioxley <brian.oxley@gmail.com>
2024-08-26 22:52:00 +0100

    Ok Now the API seems to be  working..

---
+++ Controllers/PropertiesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LetEase.Infrastructure.Data;
+using LetEase.Domain.Entities;
+
+namespace LetEase.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PropertiesController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public PropertiesController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/Properties
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Property>>> GetProperties()
+		{
+			return await _context.Properties.ToListAsync();
+		}
+
+		// GET: api/Properties/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Property>> GetProperty(int id)
+		{
+			var property = await _context.Properties.FindAsync(id);
+
+			if (property == null)
+			{
+				return NotFound();
+			}
+
+			return property;
+		}
+
+		// POST: api/Properties
+		[HttpPost]
+		public async Task<ActionResult<Property>> PostProperty(Property property)
+		{
+			_context.Properties.Add(property);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetProperty", new { id = property.Id }, property);
+		}
+	}
+}
+
+
--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,20 @@
-FROM mcr.microsoft.com/dotnet/aspnet:8.0-nanoserver-1809 AS base
-WORKDIR /app
-EXPOSE 80
-EXPOSE 443
-
-FROM mcr.microsoft.com/dotnet/sdk:8.0-nanoserver-1809 AS build
+# Use the .NET SDK image for Windows
+FROM mcr.microsoft.com/dotnet/sdk:8.0-windowsservercore-ltsc2019 AS build
 WORKDIR /src
 COPY ["LetEase.API/LetEase.API.csproj", "LetEase.API/"]
 COPY ["LetEase.Application/LetEase.Application.csproj", "LetEase.Application/"]
 COPY ["LetEase.Infrastructure/LetEase.Infrastructure.csproj", "LetEase.Infrastructure/"]
 COPY ["LetEase.Domain/LetEase.Domain.csproj", "LetEase.Domain/"]
-COPY . .
 RUN dotnet restore "LetEase.API/LetEase.API.csproj"
+COPY . .
 WORKDIR "/src/LetEase.API"
 RUN dotnet build "LetEase.API.csproj" -c Release -o /app/build
 
 FROM build AS publish
 RUN dotnet publish "LetEase.API.csproj" -c Release -o /app/publish
 
-FROM base AS final
+# Use the ASP.NET runtime image for Windows
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-windowsservercore-ltsc2019 AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
 ENTRYPOINT ["dotnet", "LetEase.API.dll"]
